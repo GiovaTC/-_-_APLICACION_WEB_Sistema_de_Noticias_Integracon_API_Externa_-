c1a7d0060d144b21c03cbc2acec1177ed3154e07
gtapiero <giovanny.tapiero@pi.edu.co>
2026-04-14 11:47:29 -0500

    commit - Noticia / .

--- src/main/java/com/noticias/app/model/Noticia.java
+++ src/main/java/com/noticias/app/model/Noticia.java
@@ -1,4 +1,43 @@
 package com.noticias.app.model;
 
 public class Noticia {
+
+    private String titulo;
+    private String descripcion;
+    private String url;
+    private  String fuente;
+
+    // getters & setters .
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getFuente() {
+        return fuente;
+    }
+
+    public void setFuente(String fuente) {
+        this.fuente = fuente;
+    }   
 }
